2d996d664ee866f5c52aa7fed35112817b4b565d
Luke Klymshyn <unseenmountain@gmail.com>
2020-01-17 18:37:45 -0800

    Created my server with the routes to the HTML

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = express();
+
+var PORT = process.env.PORT || 3306;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening to PORT: " + PORT)
+})
